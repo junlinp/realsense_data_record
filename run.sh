558bf841c8261efb3eba66f70550ede7a52bbacb
junlinp <junlinp@deepmirror.com>
2025-06-09 14:41:01 +0800

    update scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 docker build -t realsense_record .
 
+docker rm -f realsense_record
 docker run \
-	-it \
-	--rm \
+	-d \
+	--name realsense_record \
 	--env DISPLAY=$DISPLAY \
 	--env QT_X11_NO_MITSHM=1 \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix" \
@@ -16,7 +17,7 @@ docker run \
 	-v .:/workspace \
 	realsense_record /bin/bash -c 'ros2 launch realsense2_camera rs_launch.py   rgb_camera.profile:=640x480x30   depth_module.profile:=640x480x30 enable_infra1:=true enable_infra2:=true depth_module.emitter_enabled:=false depth_module.enable:=true'
 
-
+docker exec -it realsense_record bash -c 'source /opt/ros/foxy/setup.sh && cd /workspace && ros2 bag record -a'
 
 #ros2 launch realsense2_camera rs_launch.py   rgb_camera.profile:=640x480x30   depth_module.profile:=640x480x30 enable_infra1:=true enable_infra2:=true depth_module.emitter_enabled:=false depth_module.enable:=true
 
