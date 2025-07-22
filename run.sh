5db25de2f25443fb5ebe66db3e618eabaee828f8
junlinp <junlinp@deepmirror.com>
2025-07-22 17:42:08 +0800

    update run.sh to disable emitter auto

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-docker image inspect realsense_record >/dev/null 2>&1 || docker build -t realsense_record .
+if ! docker image inspect realsense_record >/dev/null 2>&1; then
+  echo "Docker image 'realsense_record' not found. Building..."
+  docker build -t realsense_record .
+else
+  echo "Docker image 'realsense_record' already exists."
+fi
 
 
 docker rm -f realsense_record
@@ -16,9 +21,6 @@ docker run \
 	--device-cgroup-rule='c 189:* rmw' \
 	--privileged \
 	-v .:/workspace \
-	realsense_record /bin/bash -c 'ros2 launch realsense2_camera rs_launch.py   rgb_camera.profile:=640x480x30   depth_module.profile:=640x480x30 enable_infra1:=true enable_infra2:=true depth_module.emitter_enabled:=false depth_module.enable:=true'
+	realsense_record /bin/bash -c 'ros2 launch realsense2_camera rs_launch.py  depth_module.profile:=640x360x15 enable_infra1:=true enable_infra2:=true enable_sync:=true enable_accel:=true enable_gyro:=true accel_fps:=200 depth_module.emitter_enabled:=0 depth_module.enable:=true & sleep 5 && ros2 param set /camera/camera depth_module.emitter_enabled 0 && tail -f /dev/null'
 
-docker exec -it realsense_record bash -c 'source /opt/ros/foxy/setup.sh && cd /workspace && ros2 bag record -a'
-
-#ros2 launch realsense2_camera rs_launch.py   rgb_camera.profile:=640x480x30   depth_module.profile:=640x480x30 enable_infra1:=true enable_infra2:=true depth_module.emitter_enabled:=false depth_module.enable:=true
-
+docker exec -it realsense_record bash -c 'source /opt/ros/foxy/setup.sh && cd /workspace && ros2 bag record /camera/infra1/camera_info /camera/infra1/image_rect_raw /camera/infra1/metadata /camera/infra2/camera_info /camera/infra2/image_rect_raw /camera/infra2/metadata /camera/extrinsics/depth_to_infra1 /camera/extrinsics/depth_to_infra2 /camera/accel/sample /camera/gyro/sample'
